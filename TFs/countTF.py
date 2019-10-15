5b18040acc6357dce54028aa5856132da1df095b
Yilei Huang <yh362@cornell.edu>
2019-10-15 10:05:34 -0400

    TFs summary for tomato and tabacum

--- TFs/countTF.py
+++ TFs/countTF.py
@@ -1,27 +1,37 @@
+import re
+import argparse
 import pandas as pd
 import numpy as np
-import argparse
 
 parser = argparse.ArgumentParser(
-        description='Path to the output from hmmscan formatted as parsable table')
+    description='Path to the output from hmmscan formatted as parsable table')
 parser.add_argument('-f', action="store", dest="f", type=str, required=True)
 args = parser.parse_args()
 
+res = []
+p = re.compile(r"\s+")
+with open(args.f) as f:
+    for line in f.read().splitlines():
+        if not line.startswith('#'):
+            res.append(p.split(line, maxsplit=18))
 
-df = pd.read_csv(args.f, header=None, comment='#', sep='\s{18}')
-print(df)
-df.columns = ['targetName','targetAccession','queryName','queryAccession',
-              'E-value1','score1','bias1','E-value2', 'score2', 'bias2',
-              'exp','reg','clu','ov','env','dom','rep','inc','targetDescription']
+df = pd.DataFrame(columns=['targetName', 'targetAccession', 'queryName', 'queryAccession',
+                           'E-value1', 'score1', 'bias1', 'E-value2', 'score2', 'bias2',
+                           'exp', 'reg', 'clu', 'ov', 'env', 'dom', 'rep', 'inc', 'targetDescription'],
+                  data=res)
 
 TFs = set(df['targetName'])
 
-out = df.DataFrame(columns=['TF family name', 'number of TFs','list of TFs'])
+
+output = []
 for tf in TFs:
     num = np.sum(np.array(df['targetName'] == tf))
-    listQuery = df[df['targetName' == tf]]['queryName']
-    out = out.append([tf, num, listQuery])
+    listQuery = df[df['targetName'] == tf]['queryName'].tolist()
+    #print(listQuery)
+    #out = out.append([tf, num, listQuery])
+    output.append([tf, num, ','.join(listQuery)])
 
+out = pd.DataFrame(columns=['TF family name', 'number of TFs','list of TFs'], data=output)
 out.to_csv(f"./{args.f}.summary.csv", sep=',', header=True)
 
 
